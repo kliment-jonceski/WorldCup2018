34316a4ecc3aa8ae8ba8f85bb5257a327de96dfd
kliment <kliment.jonceski@gmail.com>
2017-08-07 17:53:03 +0200

    Initial commit of the application.
    Changed git ignore file.
    Added all initial android classes for the project

---
+++ app/src/main/java/jonceski/kliment/worldcup2018/App.java
@@ -0,0 +1,10 @@
+package jonceski.kliment.worldcup2018;
+
+import android.app.Application;
+
+/**
+ * Created by kliment on 8/7/2017.
+ */
+
+public class App extends Application {
+}
---
+++ app/src/main/java/jonceski/kliment/worldcup2018/ui/features/splash_screen_feature/SplashActivity.java
@@ -0,0 +1,15 @@
+package jonceski.kliment.worldcup2018.ui.features.splash_screen_feature;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import jonceski.kliment.worldcup2018.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
